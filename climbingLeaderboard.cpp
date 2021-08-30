e339491add3f096faae85bf6b5386136cc998ff8
Vitor Mendes <vitorsmendscod@gmail.com>
2021-08-29 21:04:42 -0300

    fixing some problems. response continue invert

--- climbingLeaderboard.cpp
+++ climbingLeaderboard.cpp
@@ -58,20 +58,35 @@ struct ll *insert_front(struct ll *front, int num, int position){
 int search(struct ll *front, int val){
 
     //Iterate through the linked list until we reach the last node
-    while (front->next != NULL)
-    {
-
-        if (front->position == val) // value was found in list return 1
+    /*
+    while (front->next != NULL){
+    cout << "\n" << front->data << "\n";
+    
+        if (front->position == val){ 
+            cout << "\nencontrei na pos: " << front->position << " data: "<< front->data <<"\n";
             return front->data;
+            
+        }
+
+        front = front->next;
+    }*/
+    
+    for(int i = 1; i <= tam_ranked; i++){
+        cout << "\n" << front->data << "\n";
+    
+        if (front->position == val){ 
+            cout << "\nencontrei na pos: " << front->position << " data: "<< front->data <<"\n";
+            return front->data;
+            
+        }
 
         front = front->next;
     }
 
-    return 0; // value was not found in list return 0
+    return 99; 
 }
 
-void print(struct ll *front, int tipo)
-{
+void print(struct ll *front, int tipo){
     //Iterate through the linked list until we reach the last node
     if(tipo == 1){
         //printf("\ntamanho ranked %d\n", tam_ranked);
@@ -118,21 +133,42 @@ struct ll *inserting(struct ll *rank, int tipo, int tam){
     getline(cin, points_string);
     points_int = divide(points_string);
     
+
     if(tipo == 1){
         tam_ranked = tam;
         int posi_r = 1;
+
         for (const auto &point : points_int){
-            if(point != res) rank = insert_front(rank, point, posi_r);
-            if(point == res) tam_ranked--;
-            res = point;
-            posi_r++;
-            aux--;
+            if(point != res){ 
+                rank = insert_front(rank, point, posi_r);
+                res = point;
+                posi_r++;
+                aux--;
+    
+            }
+            else if(point == res){
+                tam_ranked--;
+                cout << "\n-\n";
+            } 
+            
         }
+        
     }else{
         tam_player = tam;
-        for (const auto &point : points_int) {
-            rank = insert_front(rank, point, aux);
-            aux--;
+        int posi_r = 1;
+        for (const auto &point : points_int){
+            if(point != res){ 
+                rank = insert_front(rank, point, posi_r);
+                res = point;
+                posi_r++;
+                aux--;
+    
+            }
+            else if(point == res){
+                tam_player--;
+                cout << "\n-\n";
+            } 
+            
         }
     }
     
@@ -141,60 +177,106 @@ struct ll *inserting(struct ll *rank, int tipo, int tam){
 
 
 vector<int> compare(struct ll *ranked, struct ll *player){
+    cout << "\n----- ranked:  \n";
     print(ranked, 1);
+    cout << "\n----- player:  \n";
     print(player, 2);
-    int i_ranked, i_player;
+    int aux_ranked, aux_player;
     vector<int> pos_player;
-    cout << "tam: " << tam_ranked << "\n";
+    /*
+    for(int i = 1; i <= tam_player; i++){
+        aux_player = search(player, i);
+        cout << "\ni = " << i << " num: " << aux_player;
+        for(int j = tam_ranked; j > 0; j--){
+            aux_ranked = search(player, j);
+            cout << "\nj = " << j << " num: " << aux_ranked;
+        }
+    }*/
+    
+    int i_ranked, i_player;
     
     for(int j = tam_player; j > 0; j--){
-    cout << "\n-------- pos_player: "<< j<<"---------\n"; 
+        //cout << "\n-------- pos_player: "<< j<<"---------\n"; 
         i_player = search(player, j);
         
         for(int i = tam_ranked; i > 0; i--){
             
-            cout << "\ncomparando p = "<< i_player << " com r = "<< i_ranked<<"\n";
+            //cout << "\ncomparando p = "<< i_player << " com r = "<< i_ranked<<"\n";
             
-            cout << "\n----pos_rank: "<< i<<"-----n/";
+            //cout << "\n----pos_rank: "<< i<<"-----n/";
             i_ranked = search(ranked, i);
-            cout << "\n vez:"<< i<<"\n";
+            //cout << "\n vez:"<< i<<"\n";
             if(i_player > i_ranked){ 
-                cout << "maior";
+                //cout << "maior";
                 if(i = 0){ 
                     pos_player.push_back(1);
                 }
             }
             else if(i_player = i_ranked){
-                cout << "igual";
+                //cout << "igual";
                 pos_player.push_back(i);
             }
             else if(i_player < i_ranked){
-                cout << "é menor";
+                //cout << "é menor";
+                pos_player.push_back(i+1);
+            }
+             
+        }
+    }    
+
+    return pos_player;
+}
+
+/*
+    for(int j = tam_player; j > 0; j--){
+        //cout << "\n-------- pos_player: "<< j<<"---------\n"; 
+        i_player = search(player, j);
+        
+        for(int i = tam_ranked; i > 0; i--){
+            
+            //cout << "\ncomparando p = "<< i_player << " com r = "<< i_ranked<<"\n";
+            
+            //cout << "\n----pos_rank: "<< i<<"-----n/";
+            i_ranked = search(ranked, i);
+            //cout << "\n vez:"<< i<<"\n";
+            if(i_player > i_ranked){ 
+                //cout << "maior";
+                if(i = 0){ 
+                    pos_player.push_back(1);
+                }
+            }
+            else if(i_player = i_ranked){
+                //cout << "igual";
+                pos_player.push_back(i);
+            }
+            else if(i_player < i_ranked){
+                //cout << "é menor";
                 pos_player.push_back(i+1);
             }
              
         }
     }
-    return pos_player;
-}
+    */
 
 main(void){
 
     struct ll *ranked, *player;
     ranked = NULL; // An empty linked list
     player = NULL;
+    
+    
     string num = "3"; 
     vector<int> solution;
-    //getline(cin, num);
+    getline(cin, num);
     int m = stoi(num);
     ranked = inserting(ranked, 1, m);
-    //getline(cin, num);
+    getline(cin, num);
     int n = stoi(num);
-    //player = inserting(player, 2, n);
-    //print(ranked, 1);
-    //print(player, 2);
+    player = inserting(player, 2, n);
+    print(ranked, 1);
+    print(player, 2);
     solution = compare(ranked, player);
-    
+    cout << "tam: " << tam_ranked << "\n";
     for (const auto &p : solution) {
         cout << p;
     }
